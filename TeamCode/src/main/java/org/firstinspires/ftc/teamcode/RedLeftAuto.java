bfb785cefea9d6075eafc35948b997c03b57a5ca
Artem-Drohobytsky <88010763+Artem-Drohobytsky@users.noreply.github.com>
2022-10-30 14:15:44 -0700

    rr dependecies

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedLeftAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedLeftAuto.java
@@ -4,15 +4,15 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.har
 
 import com.acmerobotics.roadrunner.*;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.noahbres.meepmeep.MeepMeep;
-import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
-import com.noahbres.meepmeep.roadrunner.SampleMecanumDrive;
-import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
-import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequence;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
+@Autonomous(name = "Red Left Auto")
 public class RedLeftAuto extends LinearOpMode {
-    public void runOpMode() throws InterruptedException{
+    public void runOpMode() {
 
         Pose2d startPose = new Pose2d(0, 0, 0);
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
@@ -20,7 +20,7 @@ public class RedLeftAuto extends LinearOpMode {
         TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
                 .lineToLinearHeading(new Pose2d(-12, -60.5, Math.toRadians(90)))
                 .lineToLinearHeading(new Pose2d(-10, -10, Math.toRadians(45)))
-                .build()
+                .build();
 
         waitForStart();
 
