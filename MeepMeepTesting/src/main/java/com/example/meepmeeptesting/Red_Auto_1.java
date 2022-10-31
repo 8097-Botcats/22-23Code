68105e00d7ab42aa18bc9d3a74d3488d8391349e
Artem-Drohobytsky <88010763+Artem-Drohobytsky@users.noreply.github.com>
2022-10-30 17:53:04 -0700

    using apriltag pipeline with auto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Red_Auto_1.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Red_Auto_1.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -14,8 +15,8 @@ public class Red_Auto_1 {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-34.5, -60.5, Math.toRadians(90)))
-                                .lineToLinearHeading(new Pose2d(-12, -60.5, Math.toRadians(90)))
-                                .lineToLinearHeading(new Pose2d(-10, -10, Math.toRadians(45)))
+                                .lineTo(new Vector2d(-12, -60.5))
+                                .lineTo(new Vector2d(-12, -34))
                                 .build()
                 );
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedLeftAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedLeftAuto.java
@@ -4,28 +4,88 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.har
 
 import com.acmerobotics.roadrunner.*;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.sun.tools.javac.util.BasicDiagnosticFormatter;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+import java.util.ArrayList;
 
 @Autonomous(name = "Red Left Auto")
 public class RedLeftAuto extends LinearOpMode {
     public void runOpMode() {
+        OpenCvWebcam webcam;
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
 
-        Pose2d startPose = new Pose2d(0, 0, 0);
+        AprilTag pipeline;
+
+        pipeline = new AprilTag();
+        webcam.setPipeline(pipeline);
+
+        int[] IDsofInterest = {0, 1, 2};
+        int detectionID = 0;
+        boolean tagFound = false;
+
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+        Pose2d startPose = new Pose2d(-34.5, -60.5, Math.toRadians(90));
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
-                .lineToLinearHeading(new Pose2d(-12, -60.5, Math.toRadians(90)))
-                .lineToLinearHeading(new Pose2d(-10, -10, Math.toRadians(45)))
+        TrajectorySequence trajSeqLeft = drive.trajectorySequenceBuilder(startPose)
+                .lineTo(new Vector2d(-59, -60.5))
+                .lineTo(new Vector2d(-59, -34))
                 .build();
 
-        waitForStart();
+        TrajectorySequence trajSeqCenter = drive.trajectorySequenceBuilder(startPose)
+                .lineTo(new Vector2d(-34.5, -34))
+                .build();
+
+        TrajectorySequence trajSeqRight = drive.trajectorySequenceBuilder(startPose)
+                .lineTo(new Vector2d(-12, -60.5))
+                .lineTo(new Vector2d(-12, -34))
+                .build();
+
+        while(!isStopRequested() || !isStarted()){
+            ArrayList<AprilTagDetection> detections = pipeline.getLatestDetections();
+            for(AprilTagDetection detection : detections){
+                detectionID = detection.id;
+            }
+        }
 
         if (!isStopRequested()) {
-            drive.followTrajectorySequence(trajSeq);
+            if(detectionID == 0){
+                drive.followTrajectorySequence(trajSeqLeft);
+            }
+            if(detectionID == 1){
+                drive.followTrajectorySequence(trajSeqCenter);
+            }
+            if(detectionID == 2){
+                drive.followTrajectorySequence(trajSeqRight);
+            }
+
         }
 
     }
